6403e04adf8b4b396390778d8a968cc4ed084903
nosvagor <nosvagor@gmail.com>
2023-03-15 12:42:26 -0700

    lualine theme added

---
+++ lua/lualine/themes/vagari.lua
@@ -0,0 +1,27 @@
+local p = require("vagari.palette")
+
+local vagari = {
+	normal = {
+		a = { bg = p.blu_2, fg = p.drk_0 },
+		b = { fg = p.blu_2, bg = p.glc_1 },
+		c = { fg = p.glc_4, bg = p.drk_0 },
+	},
+	insert = {
+		a = { bg = p.grn_2, fg = p.drk_0 },
+		b = { fg = p.grn_2, bg = p.glc_1 },
+	},
+	command = {
+		a = { bg = p.orn_2, fg = p.drk_0 },
+		b = { fg = p.orn_3, bg = p.glc_1 },
+	},
+	visual = {
+		a = { bg = p.orn_2, fg = p.drk_0 },
+		b = { fg = p.prp_2, bg = p.glc_1 },
+	},
+	replace = {
+		a = { bg = p.orn_2, fg = p.drk_0 },
+		b = { fg = p.emr_2, bg = p.glc_1 },
+	},
+}
+
+return vagari
